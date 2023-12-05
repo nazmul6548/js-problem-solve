4b78d2bbebea96ced15876a2e9c8871064560982
nazmul6548 <nazmul6548@gmail.com>
2023-12-05 23:04:10 +0600

    reduce right

--- reduce.js
+++ reduce.js
@@ -1,18 +1,27 @@
-const array =[1,2,3,4,5];
-const result =array.reduce((prevValue, array) => {
-return prevValue + array} ,0);
+// const array =[1,2,3,4,5];
+// const result =array.reduce((prevValue, array) => {
+// return prevValue + array} ,0);
 // console.log(result);
 
 
 
 // what is this?
-(() => {
-    console.log(typeof arguments);
-}) ();
+// (() => {
+//     console.log(typeof arguments);
+// }) ();
 // answer is object ,but how do it work  it s dont know
 
-const numbers =[1,2,3];
-const method = numbers.reduce((previousvalue,currentvalue) => {
-    return currentvalue -previousvalue;
-},2);
-console.log(method);
+// const numbers =[1,2,3];
+// const method = numbers.reduce((previousvalue,currentvalue) => {
+//     return currentvalue -previousvalue;
+// },2);
+// console.log(method);
+
+
+const arr = [1,2,7,9,6,7];
+arr.reduceRight((acc,cur) => {
+    console.log(acc);
+    console.log(cur);
+    
+    
+});
