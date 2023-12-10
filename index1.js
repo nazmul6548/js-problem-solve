8338746c100fc6753957df68349bfcc4caf58025
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2023-12-10 18:04:18 +0600

    issues

--- index1.js
+++ index1.js
@@ -1,6 +1,15 @@
-console.log(1);
-setTimeout(function() {
-  console.log(2);
-},0);
-console.log(3);
+// console.log(1);
+// setTimeout(function() {
+//   console.log(2);
+// },0);
+// console.log(3);
 
+// let x = 5;
+// let y = 10;
+// let z =5;
+// console.log(x===z);
+
+function test(a,a) {
+  console.log(a);
+}
+test(10);
